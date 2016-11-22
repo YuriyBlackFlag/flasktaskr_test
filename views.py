c61b30c83064060a3589ba60ce635226a4caa451
RA <sfsdfs@mail.com>
2016-11-22 22:02:07 +0200

    init app

--- views.py
+++ views.py
@@ -3,6 +3,7 @@
 from flask import Flask, render_template, request, session, \
     flash, redirect, url_for, g
 from functools import wraps
+from forms import AddTaskForm
 import cx_Oracle
 
 app = Flask(__name__)
@@ -48,35 +49,77 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/main")
+@app.route("/tasks")
 @login_required
-def main():
+def tasks():
     g.db = connect_db()
     cur = g.db.cursor()
-    cur.execute("SELECT * FROM POSTS")
-    posts = [dict(title=row[0], post=row[1]) for row in
-             cur.fetchall()]
+    cur.execute("SELECT name, due_date, priority, id FROM TASKS WHERE STATUS = 1 ORDER BY id DESC")
+    open_tasks = [
+        dict(name=row[0], due_date=row[1], priority=row[2],
+             id=row[3]) for row in cur.fetchall()
+        ]
+    cur.execute("SELECT name, due_date, priority, id FROM TASKS WHERE STATUS = 0")
+    closed_tasks = [
+        dict(name=row[0], due_date=row[1], priority=row[2],
+             id=row[3]) for row in cur.fetchall()
+        ]
     cur.close()
     g.db.close()
-    return render_template("main.html", posts=posts)
+    return render_template("tasks.html", form=AddTaskForm(request.form),
+                           open_tasks=open_tasks,
+                           closed_tasks=closed_tasks)
 
 
 @app.route("/add", methods=["POST"])
-def add():
-    title = request.form["title"]
-    post = request.form["post"]
-    if not title or not post:
-        flash("All fields are required")
-        return redirect(url_for("main"))
+@login_required
+def new_task():
+    g.db = connect_db()
+    cur = g.db.cursor()
+    name = request.form['name']
+    date = request.form['due_date']
+    priority = request.form['priority']
+    if not name or not date or not priority:
+        flash("All fields are required. Please try again.")
+        return redirect(url_for('tasks'))
     else:
-        g.db = connect_db()
-        cur = g.db.cursor()
-        cur.execute("insert into posts (title, post) values (:1,:2)", [request.form['title'], request.form['post']])
-        cur.close()
+        cur.execute("select max(id) from tasks")
+        max_id = cur.fetchone()
+        cur.execute('insert into tasks (id, name, due_date, priority, status) \
+        values(:1, :2, :3, :4,  1)',
+                    [max_id[0] + 1, request.form['name'], request.form['due_date'], request.form['priority']])
         g.db.commit()
+        cur.close()
         g.db.close()
-        flash('New entry was successfully posted!')
-        return redirect(url_for('main'))
+        flash('New entry was successfully posted. Thanks.')
+        return redirect(url_for('tasks'))
+
+
+@app.route('/complete/<int:task_id>/')
+@login_required
+def complete(task_id):
+    g.db = connect_db()
+    cur = g.db.cursor()
+    cur.execute('update tasks set status = 0 where id=' + str(task_id))
+    g.db.commit()
+    cur.close()
+    g.db.close()
+    flash('The task was marked as complete.')
+    return redirect(url_for('tasks'))
+
+
+# Delete Tasks
+@app.route('/delete/<int:task_id>/')
+@login_required
+def delete_entry(task_id):
+    g.db = connect_db()
+    cur = g.db.cursor()
+    cur.execute('delete from tasks where id=' + str(task_id))
+    g.db.commit()
+    cur.close()
+    g.db.close()
+    flash('The task was deleted.')
+    return redirect(url_for('tasks'))
 
 
 if __name__ == '__main__':
